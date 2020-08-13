9c7f5185bcb91e4f8be80f11e0d409244e2fc907
Dev Karan Pradhan <dvpradhan312@gmail.com>
2020-08-13 13:47:48 +0530

    assignment 2

---
+++ src/Assignment2/ProjectCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Media, Container, Row, Col } from 'reactstrap';
+const ProjectCard = (props) => {
+    const { image_url, name, description } = props.project
+    return (
+        <Media>
+            <Media left href="#" className="mr-2">
+                <Media object style={ { maxHeight: 128, maxWidth: 128} } src={image_url} alt="Generic placeholder image"  />
+            </Media>
+            <Media body>
+                <Media heading>
+                    {name}
+                </Media>
+                    {description}
+            </Media>
+        </Media> 
+    );
+};
+export default ProjectCard;
---
+++ src/Assignment2/ProjectList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import projectList from './projectList.json'
+import ProjectRow from './ProjectRow';
+import ProjectCard from './ProjectCard';
+import { Container, Row, Col } from 'reactstrap';
+
+const ProjectList = () => {
+    // let arr = [];
+    // for( let i=0; i<projectList.length; i+=2){
+    //     arr.push(<ProjectRow project1 = {projectList[i]} project2 = {projectList[i+1]}/>);
+    // }
+    // return (arr);
+    //  {if (props.project2 !== false) {<Col xs="6"><ProjectCard project = {props.project2}/></Col>}}
+    let flag = true;
+    let project1, project2;
+    const rowLen = projectList.length;
+    return(
+        <Container>
+            {
+                projectList.map((project, i) => {
+                    if (flag === true) {
+                        project1 = project;
+                        flag = false;
+                        if (rowLen === i + 1) {
+                            return (<Container className="container pt-3">
+                                <Row><Col><ProjectCard project = {project1} /></Col></Row>
+                                </Container>);
+                        }
+                    }
+                    else {
+                        project2 = project;
+                        flag = true;
+                        return (<ProjectRow project1 = {project1} project2 = {project2}/>);
+                    }
+                })
+            }
+        </Container>
+    );
+}
+export default ProjectList;
